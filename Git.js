fea7d2a49bd92eb59d39438f74babf073970b26d
Khushal Vaghasiya <khushalvaghasiya0@gmail.com>
2025-04-10 15:57:14 +0530

    Initial code added

---
+++ Git.js
@@ -0,0 +1,3 @@
+function sayhetlo() {
+    console.log( 'Hello')
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const age = 20;
+const name = "khushal"
+const dob = 2006
+
+function sayname() {
+    console.log("My name is "+name);
+}
